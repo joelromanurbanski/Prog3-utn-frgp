c0d77b58aba42755551e5888ccbbb52ae6c6f7a5
joel <joeluki2000@gmail.com>
2025-09-08 21:46:43 -0300

    primer version menu

---
+++ tp-winform-equipo-3b/tp-winform-equipo-3B/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tp_winform_equipo_3B
+{
+    public partial class Catalogo : Form
+    {
+        public Catalogo()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Menu_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
